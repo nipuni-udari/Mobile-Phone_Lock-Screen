963898fa4b9207506283ffa7ea51f44b20c285fb
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 18:53:28 +0530

    import recat native components

---
+++ app/screens/ApplicationLocked.js
@@ -0,0 +1,12 @@
+import React, {Component, useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  Dimensions,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
