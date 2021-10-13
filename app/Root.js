8ad0b4e57eeca407a37739edf2282a9e10543e2e
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 18:52:41 +0530

    Add ApplicationLocked screen to root

--- app/Root.js
+++ app/Root.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 
-import LockScreenPasscode from './screens/LockScreenPasscode';
+//import LockScreenPasscode from './screens/LockScreenPasscode';
+import ApplicationLocked from './screens/ApplicationLocked';
 
 export default class Root extends Component {
   render() {
-    return <LockScreenPasscode />;
+    //return <LockScreenPasscode />;
+    return <ApplicationLocked />;
   }
 }
