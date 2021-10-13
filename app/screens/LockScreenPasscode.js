757e85aa2d794f9af3c7478b1b43b363ec35803c
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 14:53:38 +0530

    import react components to LockScreen

---
+++ app/screens/LockScreenPasscode.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  Image,
+  Alert,
+  TouchableHighlight,
+} from 'react-native';
