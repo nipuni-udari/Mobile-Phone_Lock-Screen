8e1931e3ef4f5bd5b0c79e8f0f1621bb9037c8bd
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 18:39:51 +0530

    pins are defined

--- app/screens/LockScreenPasscode.js
+++ app/screens/LockScreenPasscode.js
@@ -43,6 +43,20 @@ class LockScreenPasscode extends Component {
       }
       this.setState({passcode: tempCode});
     };
+
+    render() {
+      let numbers = [
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4},
+        {id: 5},
+        {id: 6},
+        {id: 7},
+        {id: 8},
+        {id: 9},
+        {id: 0},
+      ];
  
 export default LockScreenPasscode;
 
