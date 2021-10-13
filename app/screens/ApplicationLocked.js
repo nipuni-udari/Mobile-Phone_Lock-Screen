7893f9909f43efd665dd6ec697b339cda846e6c6
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 18:56:01 +0530

    Add screen viewing elements to View component

--- app/screens/ApplicationLocked.js
+++ app/screens/ApplicationLocked.js
@@ -12,13 +12,35 @@ import {
 } from 'react-native';
 
 export default class ApplicationLocked extends Component {
-    render() {
-      return (
-        <SafeAreaView style={styles.container}>
-          <StatusBar barStyle="light-content" />
-          <View
-            style={{
-              marginTop: 100,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View
+          style={{
+            marginTop: 100,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View>
+            <Text style={styles.Text}> Maximum attempts reached</Text>
+          </View>
+          <View style={styles.image}>
+            <Image source={require('../assets/images/Lock.jpeg')} />
+          </View>
+          <View>
+            <Text style={styles.Text2}>
+              To protect your information, access has been locked for 5 minutes.
+              Come back later and try again.
+            </Text>
+          </View>
+          <TouchableOpacity onPress={() => this}>
+            <View style={styles.Text3View}>
+              <Text style={styles.Text3}> Quit</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
