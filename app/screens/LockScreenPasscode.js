9a7576a2932e1805b92d5f2460fc218825d74651
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-10-13 16:55:40 +0530

    create the style sheet

--- app/screens/LockScreenPasscode.js
+++ app/screens/LockScreenPasscode.js
@@ -12,9 +12,20 @@ import {
 } from 'react-native';
 
 class LockScreenPasscode extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        passcode: ['', '', '', ''],
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      passcode: ['', '', '', ''],
+    };
+  }
+}
+
+export default LockScreenPasscode;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+  },
+});
